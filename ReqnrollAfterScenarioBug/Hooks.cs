ea02958747b96a7ef6f5f1671a42b6118a3327ba
PaysPlat <paysplat@gmail.com>
2025-10-07 22:54:43 +0200

    Create ReqnrollAfterScenarioBug test project

---
+++ ReqnrollAfterScenarioBug/Hooks.cs
@@ -0,0 +1,22 @@
+﻿using Reqnroll;
+
+namespace ReqnrollAfterScenarioBug;
+
+[Binding]
+public static class Hooks
+{
+
+    [BeforeScenario]
+    private static void BeforeScenario()
+    {
+        Console.WriteLine("Log: BeforeScenario");
+    }
+
+    [AfterScenario]
+    private static void AfterScenario()
+    {
+        Console.WriteLine("Log: AfterScenario");
+        
+    }
+    
+}
---
+++ ReqnrollAfterScenarioBug/Steps/TestSteps.cs
@@ -0,0 +1,19 @@
+﻿using Reqnroll;
+
+namespace ReqnrollAfterScenarioBug.Steps;
+
+[Binding]
+public class TestSteps
+{
+    [When("I finish scenario with inconclusive status")]
+    public void WhenIFinishScenarioWithInconclusiveStatus()
+    {
+        Assert.Inconclusive($"Inconclusive scenario{Environment.NewLine}");
+    }
+
+    [When("I call normal step")]
+    public void WhenICallNormalStep()
+    {
+        Console.WriteLine("Log: Normal step");
+    }
+}
